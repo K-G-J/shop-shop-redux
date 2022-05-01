655892cd8010cf70e2e155a852ff63fa4695168c
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-01 19:47:13 -0400

    redeploy to heroku

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`📚API server running on port ${PORT}!`);
+    console.log(`🚀Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
